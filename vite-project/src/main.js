422f21546b874d20b211d03aeb418904734286ea
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2026-01-07 11:05:23 +0530

    prototype testing

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -3,13 +3,13 @@ const navToggle = document.querySelectorAll(".nav-toggle")
 
 Array.from(navToggle).forEach(btn => btn.addEventListener('click', () => {
     console.log(primaryNav.style.left);
-    
+
     if (primaryNav.hasAttribute("data-visible")) {
         btn.setAttribute("aria-expanded", false)
     } else {
         btn.setAttribute("aria-expanded", true);
     }
-primaryNav.toggleAttribute("data-visible")
+    primaryNav.toggleAttribute("data-visible")
 }))
 
 
@@ -20,14 +20,33 @@ const categoryItems = document.querySelectorAll('.category-item')
 const app = document.querySelector(".app")
 
 const loadPage = async (page) => {
-    const res = await fetch(page) 
+    const res = await fetch(page)
     const data = await res.text()
-    console.log(res);
-    
+    // console.log(res);
+
     app.innerHTML = data
-    console.log(data);
-    
+    // console.log(data);
+
+    Array.from(document.querySelectorAll(".table-box")).forEach((table) => {
+        table.addEventListener("click", () => {
+            loadPage("order/order.htm")
+
+        })
+    })
+
+    // // document.querySelector(".to-home").addEventListener("click", ()=>{
+    // //     console.log(654);
+
+    // // })
 }
 
 loadPage("home/home.htm")
-loadPage("order/order.htm")
+// loadPage("order/order.htm")
+
+app.addEventListener("click", (e) => {
+    console.log("e.target", e.target.innerHTML);
+    if (e.target.innerHTML == "Save") {
+
+        loadPage("home/home.htm")
+    }
+})
